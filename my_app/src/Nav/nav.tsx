e9c0d87d3f659b4f31dec38b1d1f9c9a7275fcef
JosephCrespin <erickandrescrespin@gmail.com>
2024-04-20 01:22:03 +0200

    new_cont

--- my_app/src/Nav/nav.tsx
+++ my_app/src/Nav/nav.tsx
@@ -2,7 +2,6 @@ import "./nav.css";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "../assets/LOGO1.png";
-import Cv from "../assets/CV_ERICK.pdf";
 
 export default function Nav() {
   return (
@@ -14,7 +13,7 @@ export default function Nav() {
         <div className="contLink">
           <a href="">About</a>
           <a href="">Proyectos</a>
-          <Link href={Cv} target="_blank" download="CV_ERICK.pdf">
+          <Link href="/cvERICK.pdf" target="_blank" download="CV_ERICK.pdf">
             Descargar cv
           </Link>
         </div>
