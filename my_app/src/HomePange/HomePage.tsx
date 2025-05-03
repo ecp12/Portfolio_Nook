ab264a003d6e858bfc68f30459f88664079a2f41
ecp12 <erickandrescrespin@gmail.com>
2025-05-03 22:44:11 +0200

    new_change

--- my_app/src/HomePange/HomePage.tsx
+++ my_app/src/HomePange/HomePage.tsx
@@ -1,10 +1,14 @@
 import '../HomePange/HomePage.css'
 import Nav from '../Nav/nav'
+
 export default function HomePage() {
- return(<div className='Home'>
-    <Nav/>
- </div>
- 
- );
- 
+  return (
+    <div className='Home'>
+      <Nav/>
+      <div className="description-section">
+        <h1>Welcome to My Portfolio</h1>
+        <p>Please provide the description content you'd like to display here</p>
+      </div>
+    </div>
+  );
 }
